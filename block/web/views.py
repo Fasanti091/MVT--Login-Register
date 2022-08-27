a8921d05232916d537f281fed1b272945c801233
Andres Suarez <santiagosuarez.99@hotmail.com>
2022-08-27 15:57:29 -0500

    comentarios completados

--- block/web/views.py
+++ block/web/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from web.models import *
 
 # Create your views here.
 def index(request):
     return render(request, "web/index.html")
 
 def comentarios(request):
-    return render(request, "web/comentarios.html")
+
+    comentarios = Posteo.objects.all()
+
+    return render(request, "web/comentarios.html", {"comentarios":comentarios})
