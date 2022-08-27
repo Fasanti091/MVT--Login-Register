5a8a7ff45885129d7a96bbb53fb320ff65ad7cfd
Andres Suarez <santiagosuarez.99@hotmail.com>
2022-08-27 15:25:38 -0500

    Admin y posteo funcionando en admin

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MVT--Login-Register
 #!!!
 Prueba del pull
+
+
+Admin de web ---> admin123 password ---> admin123
--- block/web/models.py
+++ block/web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Posteo(models.Model):
+    
+    titulo = models.CharField(max_length=100)
+    contenido = models.CharField(max_length=350)
+    fecha_publicacion = models.DateField()
+
